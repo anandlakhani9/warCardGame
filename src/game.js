a5a2899e52c6fc50ecdbd45709914b9b2418bf0e
Luke Fowles <luke.fowles15@gmail.com>
2021-10-15 21:37:26 +0100

    war with draw extension code

--- src/game.js
+++ src/game.js
@@ -2,7 +2,7 @@ const Player = require("../src/players.js");
 const Deck = require("../src/deck.js");
 
 const Game = function(player1="", player2="", isPlaying = false, deck = null, winner = null, loser = null,
-                      rounds= null) {
+                      rounds= null, war = false) {
     this.player1 = player1;
     this.player2 = player2;
     this.isPlaying = isPlaying;
@@ -10,6 +10,7 @@ const Game = function(player1="", player2="", isPlaying = false, deck = null, wi
     this.winner = winner;
     this.loser = loser;
     this.rounds = rounds;
+    this.war = war;
 }
 
 
@@ -35,29 +36,53 @@ Game.prototype.initGame = function(player1Name, player2Name){
     this.isPlaying = true;
 }
 
-//pop cards to players
+//pop single card to players
 Game.prototype.drawCard = function(){
     this.player1.giveCurrentCard();
     this.player2.giveCurrentCard();
   
 }
+
+//draw players card in instance of war
+//  Game.prototype.playWarHand = function () {
+//         if (this.war === false)
+//         {
+//             return;
+//         }
+//         this.player1.createWarHand();
+//         this.player2.createWarHand();
+//         console.log('yo');
+//         this.compareCards(this.player1.warHand.slice(0, 1), this.player2.warHand.slice(0, 1));
+// }
+
+//let counter = 0;
+
 //compare cards - and add to other's hand
 Game.prototype.compareCards = function(card1, card2) {
-    if(card1.value >= card2.value) {
+    if(card1.value > card2.value) {
         let randomNumber = Math.floor(2*Math.random());
         //console.log(randomNumber);
         if (randomNumber === 1)
         {
-            this.player1.hand.unshift(card1, card2);
+            for (let i = 0; i < this.player1.warHand.length; i++){
+                this.player1.hand.unshift(this.player1.warHand[i], this.player2.warHand[i])
+            }
+            //this.player1.hand.unshift(card1, card2);
             //this.player1.hand.unshift(card2);
         }
         else 
         {
-            this.player1.hand.unshift(card2, card1);
+            for (let i = 0; i < this.player1.warHand.length; i++){
+                this.player1.hand.unshift(this.player2.warHand[i], this.player1.warHand[i])
+            }
+            //this.player1.hand.unshift(card2, card1);
             //this.player1.hand.unshift(card2);
             
         }
-        //console.log("p1 wins round") 
+        this.player1.clearWarHand();
+        this.player2.clearWarHand();
+        console.log("p1 wins round")
+        this.war = false;
         return ("card 1 is higher")
     }
     else if(card2.value > card1.value) 
@@ -65,18 +90,31 @@ Game.prototype.compareCards = function(card1, card2) {
         let randomNumber = Math.floor(2*Math.random());
         if (randomNumber === 1)
         {
-            this.player2.hand.unshift(card1, card2);
+            for (let i = 0; i < this.player1.warHand.length; i++){
+                this.player2.hand.unshift(this.player1.warHand[i], this.player2.warHand[i])
+            }
+            //this.player2.hand.unshift(card1, card2);
             //this.player1.hand.unshift(card2);
         }
         else 
         {
-            this.player2.hand.unshift(card2, card1);
+            for (let i = 0; i < this.player1.warHand.length; i++){
+                this.player2.hand.unshift(this.player2.warHand[i], this.player1.warHand[i])
+            }
+            //this.player2.hand.unshift(card2, card1);
             //this.player1.hand.unshift(card2);
             
         }
-        //console.log("p2 wins round") 
-        return ("card 2 is higher")
-    }
+        this.player1.clearWarHand();
+        this.player2.clearWarHand();
+        console.log("p2 wins round");
+        console.log(this.player1.hand); 
+        this.war = false;
+        return ("card 2 is higher");
+     } else {
+         this.war = true;
+         console.log('lolo');
+     } 
 }
 
 Game.prototype.checkWin = function () {
@@ -85,13 +123,42 @@ Game.prototype.checkWin = function () {
         this.isPlaying = false;
         this.winner = this.player1.name;
         this.loser = this.player2.name
+        console.log('yoyo');
     }
     else if (this.player2.hand.length === 52){
         //console.log("Player 2 wins")
         this.isPlaying = false;
         this.winner = this.player2.name;
         this.loser = this.player1.name;
+        console.log('oioi');
     }
+}
+Game.prototype.warPlay = function() {
+    console.log("Hello")
+    while(this.war){
+        if(this.player1.hand.length > 1 && this.player2.hand.length > 1)
+        {
+            this.player1.createWarHand();
+            this.player2.createWarHand();
+            this.compareCards(this.player1.warHand[0], this.player2.warHand[0]) 
+        }
+        else if (this.player1.hand.length < 2){
+            for (let i = 0; i < this.player1.warHand.length; i++){
+                this.player2.hand.unshift(this.player2.warHand[i], this.player1.warHand[i])
+            };
+            this.player1.hand = [];
+            this.war = false;
+        }
+        else {
+            for (let i = 0; i < this.player1.warHand.length; i++){
+                this.player1.hand.unshift(this.player2.warHand[i], this.player1.warHand[i])
+            };
+            this.player2.hand = [];
+            this.war = false;
+        }
+    }
+    this.player1.clearWarHand();
+    this.player2.clearWarHand();
 }
 
 Game.prototype.playGame = function(){
@@ -111,6 +178,9 @@ Game.prototype.playGame = function(){
         //console.log(this.player1.currentCard);
         //console.log(this.player2.currentCard);
         this.compareCards(this.player1.currentCard, this.player2.currentCard);
+        if (this.war){
+          this.warPlay();
+        };
         //console.log("p1 has: " + this.player1.hand.length);
         //console.log(this.player1.hand);
         //console.log("p2 has: " + this.player2.hand.length);
@@ -120,7 +190,17 @@ Game.prototype.playGame = function(){
     this.rounds = roundCounter;
 }
 
+
 const game = new Game()
 game.playGame();
+console.log(game.rounds);
 
 module.exports = Game;
+
+// if player1.hand[1] > player2.hand[1]{
+//     card1 = player1.hand.pop()
+//     card2 = player1.hand.pop()
+//     card3 = player2.hand.pop()
+//     card4 = player2.hand.pop()
+//     player1.hand.unshift(card1, card2,card3, card4, player1.currentcard, player2.currentcard)
+// }
--- src/players.js
+++ src/players.js
@@ -1,12 +1,26 @@
 const Card = require("../src/card.js")
 
-const Player = function(playerName, hand, currentCard="null"){
+const Player = function(playerName, hand, currentCard="null", warHand = []){
     this.playerName = playerName;
     this.hand = hand;
     this.currentCard = currentCard;
+    this.warHand = warHand;
 }
     
-Player.prototype.giveCurrentCard = function() { this.currentCard = this.hand.pop()};
+Player.prototype.giveCurrentCard = function() { 
+    this.currentCard = this.hand.pop();
+    this.warHand.push(this.currentCard); };
+
+Player.prototype.createWarHand = function() {
+    //this.warHand.unshift(this.hand.slice(-2));
+    this.warHand.unshift(this.hand.pop());
+    this.warHand.unshift(this.hand.pop());
+    console.log(this.warHand);
+}
+
+Player.prototype.clearWarHand = function () {
+    this.warHand = [];
+}
 
 //for testing purposes
 Player.prototype.isCard = function(card) {
